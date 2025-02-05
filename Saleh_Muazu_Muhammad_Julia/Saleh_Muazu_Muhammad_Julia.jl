a304e31ef5017070edcde447a1b515983f72e612
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-05 03:28:41 -0800

    First commit

---
+++ Saleh_Muazu_Muhammad_Julia/Saleh_Muazu_Muhammad_Julia.jl
@@ -0,0 +1,6 @@
+# Julia program to display information
+
+println("Name: Saleh Muazu Muhammad")
+println("Email: salehmuazu83@gmail.com")
+println("Phone number: 08160864114")
+println("Area of Interest: Data Visualization")
