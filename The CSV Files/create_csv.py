faacdda24a580807566ddb911bf50ea7e96fe73e
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-05 03:31:05 -0800

    First commit

---
+++ The CSV Files/create_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+csv_file = [
+    ["Email", "Area_of_Interest"],
+    ["patiencemnena5996@gmail.com", "Genomics"],
+    ["ibrahimnuhupaki8@gmail.com", "System Biology"],
+    ["Ahmad Adamu Muhammad", "Metagenomics"],
+    ["Saleh Muazu Muhammad", "Data Visualization"],
+    ["maryammuhammadnawat@gmail.com", "Bioinformatics Tools Development"],
+    ["mhizzphydo@gmail.com", "Machine Learning in Bioinformatics"]
+]
+
+with open("data.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(csv_file)
+print("CSV file 'data.csv' created successfully")
