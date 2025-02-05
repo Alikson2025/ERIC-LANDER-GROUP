a899b12d41dda0667be895e5d78585ce9b174c66
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-05 03:30:00 -0800

    First commit

---
+++ Ukange_Patience_Mnena_Python/Ukange_Patience_Mnena_Python.py
@@ -0,0 +1,5 @@
+# Python program to display information
+print("Name: Ukange Patience Mnena")
+print("Phone number: 09061998983")
+print("Email: patiencemnena5996@gmail.com")
+print("Area of Interest: Genomics")
