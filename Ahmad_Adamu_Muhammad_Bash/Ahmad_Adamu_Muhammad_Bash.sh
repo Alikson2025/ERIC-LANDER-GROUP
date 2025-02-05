c758c8abeff95f6ddb8f2bdc222202ebceb2a9ce
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-05 03:26:45 -0800

    First commit

---
+++ Ahmad_Adamu_Muhammad_Bash/Ahmad_Adamu_Muhammad_Bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Bash script to display information
+
+echo "NAME: Ahmad Adamu Muhammad"
+echo "EMAIL: adamukk03@gmail.com"
+echo "PHONE NO: 09039428578"
+echo "AREA OF INTEREST: Metagenomics"
