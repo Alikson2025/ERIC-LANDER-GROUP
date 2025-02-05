2f1dbba65db42aa6c01a3c1bf973e3b158e39235
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-05 03:27:25 -0800

    First commit

---
+++ Ibrahim_Nuhu_R/Ibrahim_Nuhu_R.R
@@ -0,0 +1,6 @@
+# R program to display information
+
+cat("Name: Ibrahim Nuhu\n")
+cat("Email: ibrahimnuhupaki8@gmail.com\n")
+cat("Area of Interest: System Biology\n")
+cat("Phone Number: 08035300426\n")
